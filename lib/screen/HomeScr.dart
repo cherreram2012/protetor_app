02acc5d52e97f6b6007175d7f897531b415f1c92
Carlos Herrera <cherreram2012@gmail.com>
2020-08-25 11:57:18 -0300

    nome do aplicativo ajustado.

--- lib/screen/HomeScr.dart
+++ lib/screen/HomeScr.dart
@@ -22,7 +22,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home"),
+        title: Text("App Protetor"),
       ),
       body: Center(
         child: Center(
